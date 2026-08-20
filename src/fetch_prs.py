7df2c742b9875bf3c6f9d92f72a0567949a5f79c
Laulfred <laualfred1243@gmail.com>
2026-08-20 11:33:20 -0700

    feat: add date filtering and slim PR field extraction
    Filter on updated_at rather than created_at that cuts fetch from 379 pages to just 39. Added slim PR extraction for desired PR fields.

--- src/fetch_prs.py
+++ src/fetch_prs.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,28 +11,60 @@ if not token:
 
 #---
 
-url = 'https://api.github.com/repos/pandas-dev/pandas/pulls?per_page=100&state=all'
+url = 'https://api.github.com/repos/pandas-dev/pandas/pulls?per_page=100&state=all&sort=updated&direction=desc'
 headers = { 
     'Authorization': f'Bearer {token}'
 }
 
 results = []
 page_counter = 0
+now = datetime.now(timezone.utc)
+cutoff_date = now - timedelta(days=365) # 1 year ago
+done = False
 while url:
-    if page_counter == 5:
-        break
     response = requests.get(url, headers=headers)
-    print(response.url)
+    #print(response.url)
     if response.status_code != 200:
         print(f'Status Code: {response.status_code}')
         break
     page_data = response.json()
-    results.extend(page_data)
+    for pr in page_data:
+        recent_pr_update_time = datetime.fromisoformat(pr['updated_at'])
+        if recent_pr_update_time > cutoff_date:
+            results.append(pr)
+        elif recent_pr_update_time < cutoff_date:
+            done = True
+            break
+    if done:
+        break
     page_counter += 1
-    print(page_counter)
+    #print(page_counter)
     if 'next' in response.links:
         url = response.links['next']['url']
     else:
         url = None
 print(len(results))
-        
+
+
+# slim dict loop for desired fields
+slim_prs = []
+for pr in results:
+    slim_pr = {
+        'id' : pr['id'],
+        'number' : pr['number'],
+        'user_login' : pr['user']['login'],
+        'user_type' : pr['user']['type'],
+        'created_at' : pr['created_at'],
+        'merged_at' : pr['merged_at'],
+        'closed_at' : pr['closed_at'],
+        'updated_at' : pr['updated_at'],
+        'labels' : [label['name'] for label in pr['labels']],
+        'state' : pr['state'],
+        'title' : pr['title'],
+        'draft' : pr['draft']
+    }
+    slim_prs.append(slim_pr)
+
+print(len(slim_prs))
+print(slim_prs[0])
+
